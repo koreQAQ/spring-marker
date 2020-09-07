ea1e184933a04fbbb37b19b803c725511c536d0f
李树彬 <koreQAQ@foxmail.com>
2020-09-07 21:05:09 +0800

    init data and integartion module

--- spring-marker-data/src/main/java/org/example/spring/marker/config/SpringMarkerDataConfig.java
+++ spring-marker-data/src/main/java/org/example/spring/marker/config/SpringMarkerDataConfig.java
@@ -1,4 +1,10 @@
-package org.example.spring.marker.config;/**
+package org.example.spring.marker.config;
+
+/**
  * Created by lishubin on 2020/9/7 8:58 下午
- */public class SpringMarkerDataConfig {
+ */
+public class SpringMarkerDataConfig {
+
+
+
 }
